3fd02bea68e0dc615c9fc0d67a09ae40ec230545
Eric <ericphilipps123@gmail.com>
2022-05-22 18:46:51 -0500

    get one category working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,7 +29,26 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      atrributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+  .then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({ message: 'Category not found' });
+    }
+    else {
+      res.json(categoryData);
+    }
+  })
+  .catch((err) => {
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
